8d44d75147343c3b067325b0b65a0841a258e325
Taeho Kim <aafwu00@gmail.com>
2018-03-25 13:59:42 +0900

    rename module `netflix-evcache-server` to `netflix-evcache-sidecar`

---
+++ netflix-evcache-sidecar-spring-cloud-autoconfigure/src/main/java/com/github/aafwu00/netflix/evcache/sidecar/spring/cloud/EVCacheSidecarAutoConfiguration.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2017-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.aafwu00.netflix.evcache.sidecar.spring.cloud;
+
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.actuator.HasFeatures;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * EVCache Server Configuration that setting up {@link com.netflix.appinfo.EurekaInstanceConfig}.
+ * <p>
+ * look on {@code org.springframework.cloud.netflix.sidecar.SidecarConfiguration}
+ * look on {@code com.github.aafwu00.spring.cloud.evcache.client.MyOwnEurekaNodeListProvider}
+ *
+ * @author Taeho Kim
+ * @see EurekaClientAutoConfiguration
+ * @see com.netflix.evcache.pool.eureka.EurekaNodeListProvider
+ */
+@Configuration
+@ConditionalOnProperty(value = "evcache.sidecar.enabled", matchIfMissing = true)
+@AutoConfigureAfter(EurekaClientAutoConfiguration.class)
+@EnableConfigurationProperties(EVCacheSidecarProperties.class)
+public class EVCacheSidecarAutoConfiguration {
+    private final ConfigurableEnvironment env;
+    private final EurekaInstanceConfigBean eurekaInstanceConfigBean;
+    private final EVCacheSidecarProperties properties;
+
+    public EVCacheSidecarAutoConfiguration(final ConfigurableEnvironment env,
+                                           final EurekaInstanceConfigBean eurekaInstanceConfigBean,
+                                           final EVCacheSidecarProperties properties) {
+        this.env = requireNonNull(env);
+        this.eurekaInstanceConfigBean = requireNonNull(eurekaInstanceConfigBean);
+        this.properties = requireNonNull(properties);
+    }
+
+    @Bean
+    public HasFeatures evcacheSidecarFeature() {
+        return HasFeatures.namedFeature("Netflix EVCache Sidecar", EVCacheSidecarAutoConfiguration.class);
+    }
+
+    @PostConstruct
+    public void init() {
+        final Map<String, String> metadataMap = eurekaInstanceConfigBean.getMetadataMap();
+        putIfAbsent(metadataMap, "evcache.port", Integer.toString(properties.getPort()));
+        putIfAbsent(metadataMap, "evcache.group", properties.getGroup());
+        putIfAbsentWhenContainProperty(metadataMap, "rend.port");
+        putIfAbsentWhenContainProperty(metadataMap, "rend.batch.port");
+        putIfAbsentWhenContainProperty(metadataMap, "udsproxy.memcached.port");
+        putIfAbsentWhenContainProperty(metadataMap, "udsproxy.memento.port");
+    }
+
+    private void putIfAbsent(final Map<String, String> metadataMap, final String key, final String defaultValue) {
+        metadataMap.putIfAbsent(key, env.getProperty(key, defaultValue));
+    }
+
+    private void putIfAbsentWhenContainProperty(final Map<String, String> metadataMap, final String key) {
+        if (env.containsProperty(key)) {
+            metadataMap.putIfAbsent(key, env.getProperty(key));
+        }
+    }
+}
